8c2fe9581bd5790e57c55ec9328f4e8a534264b9
Grant Jones <grantjones94@gmail.com>
2024-01-19 18:31:40 -0800

    removing run at start

--- src/FunctionApps/BirthdayBuddyFunctionApp/DailyBirthdayFunction.cs
+++ src/FunctionApps/BirthdayBuddyFunctionApp/DailyBirthdayFunction.cs
@@ -18,12 +18,13 @@ namespace birthday_buddy_functionapp
 
         //Runs every day at 1 AM PST, expression is NCRONTAB in UTC
         [Function(nameof(DailyBirthdayFunction))]
-        public void Run([TimerTrigger("0 0 9 * * *", RunOnStartup = true)] TimerInfo timerInfo, FunctionContext context)
+        public void Run([TimerTrigger("0 0 9 * * *")] TimerInfo timerInfo, FunctionContext context)
         {
-            var logger = context.GetLogger("Function2");
             _logger.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
-            logger.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
-            logger.LogInformation($"Next timer schedule at: {timerInfo.ScheduleStatus.Next}");
+
+            //get todays birthdays
+            
+            _logger.LogInformation($"Next timer schedule at: {timerInfo.ScheduleStatus.Next}");
         
         }
 
